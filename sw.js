475066a24fcc683cd2f57c122a05f62ecec2a362
Josué Cabello <Josuecabellorosa@hotmail.com>
2026-02-28 09:23:11 +0100

    PWA lista + iconos oficiales + manifest

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "smia-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
