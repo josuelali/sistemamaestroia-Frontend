87f9f5a5bb81eb7cc149d9acc79f8e7b3648ac05
Josué Cabello <Josuecabellorosa@hotmail.com>
2026-03-21 10:01:53 +0100

    disable stale service worker cache

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
+
 self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
