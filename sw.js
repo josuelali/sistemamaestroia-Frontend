cf6aaf48f962851c5f53ce3779057de3ccf294ca
Josué Cabello <Josuecabellorosa@hotmail.com>
2026-03-21 09:54:52 +0100

    fix api generator and disable stale sw cache

--- sw.js
+++ sw.js
@@ -1,23 +1,10 @@
-const CACHE_NAME = "smia-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/manifest.webmanifest"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
-
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
